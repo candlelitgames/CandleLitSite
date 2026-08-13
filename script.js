a1e18750ec97f5f35c27cdb568df4f79d1f8e14b
coder <mercer.haselden@gmail.com>
2026-08-13 15:41:31 -0400

    bug fixes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(function() {
 
   if (window.mobileCheck()) {
     pages.addClass('mobile');
-    $('.info').css('left', '-85px');
+    $('.info').css('left', '-50px');
     $('body').addClass('font-increase');
     $('.info').addClass('font-increase');
     $('#content').addClass('font-increase');
